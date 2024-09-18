d134624e1d45e26ad7da382e3bf034ad8adee4da
quyettc1112 <TranQuyet02588x1112002@gmail.com>
2024-09-18 07:23:33 +0700

    Update UI Search Location

---
+++ app/src/main/java/com/example/tep_timeshareexchangeplatform/UI/Activity/CommonActivity/LocationActivity.kt
@@ -0,0 +1,22 @@
+package com.example.tep_timeshareexchangeplatform.UI.Activity.CommonActivity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.tep_timeshareexchangeplatform.AppConfig.BaseConfig.BaseActivity
+import com.example.tep_timeshareexchangeplatform.R
+
+class LocationActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_location)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
