fab5d0c9ccfc4f1480844ce3c6337ec2da67e4c5
quyettc1112 <TranQuyet02588x1112002@gmail.com>
2024-09-20 11:27:32 +0700

    Update Top Resort UI TabLayout

---
+++ app/src/main/java/com/example/tep_timeshareexchangeplatform/UI/Fragment/TopResortFragment/ChildFragment/TimeshareFragment/TimeshareFragment.kt
@@ -0,0 +1,31 @@
+package com.example.tep_timeshareexchangeplatform.UI.Fragment.TopResortFragment.ChildFragment.TimeshareFragment
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.tep_timeshareexchangeplatform.R
+
+class TimeshareFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = TimeshareFragment()
+    }
+
+    private val viewModel: TimeshareViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // TODO: Use the ViewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_timeshare, container, false)
+    }
+}
---
+++ app/src/main/java/com/example/tep_timeshareexchangeplatform/UI/Fragment/TopResortFragment/ChildFragment/TimeshareFragment/TimeshareViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.tep_timeshareexchangeplatform.UI.Fragment.TopResortFragment.ChildFragment.TimeshareFragment
+
+import androidx.lifecycle.ViewModel
+
+class TimeshareViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
