5f3031ac64d5694337aeb6a69957610ee9140b00
QuyetTC <133133145+quyettc1112@users.noreply.github.com>
2024-09-24 19:28:44 +0700

    Update Core List Image

---
+++ app/src/main/java/com/example/tep_timeshareexchangeplatform/UI/Activity/ResortDetailActivity/ImageListActivity.kt
@@ -0,0 +1,68 @@
+package com.example.tep_timeshareexchangeplatform.UI.Activity.ResortDetailActivity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.viewpager2.widget.ViewPager2
+import com.example.tep_timeshareexchangeplatform.AppConfig.BaseConfig.BaseActivity
+import com.example.tep_timeshareexchangeplatform.UI.Activity.ResortDetailActivity.Adapter.ImageDetailAdapter
+import com.example.tep_timeshareexchangeplatform.Common.Constant
+import com.example.tep_timeshareexchangeplatform.R
+import com.example.tep_timeshareexchangeplatform.UI.Activity.ResortDetailActivity.Adapter.ImageViewPagerAdapter
+import com.example.tep_timeshareexchangeplatform.databinding.ActivityImageListBinding
+
+class ImageListActivity : BaseActivity() {
+
+    private lateinit var binding: ActivityImageListBinding
+    private  lateinit var recycler : ImageDetailAdapter
+    private lateinit var viewPager: ImageViewPagerAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityImageListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Set up RecyclerView with LinearLayoutManager
+        setUpImageList()
+
+    }
+
+    private fun setUpImageList() {
+
+        binding.viewPager.apply {
+            viewPager = ImageViewPagerAdapter()
+            viewPager.submitList(Constant.listImage)
+            adapter = viewPager
+
+        }
+
+        binding.thumbnailRecyclerView.apply {
+            recycler = ImageDetailAdapter(binding.thumbnailRecyclerView)
+            recycler.submitList(Constant.listImage)
+            recycler.onItemClick = {
+                binding.viewPager.setCurrentItem(it, true)
+            }
+            adapter = recycler
+            layoutManager = LinearLayoutManager(this@ImageListActivity, LinearLayoutManager.HORIZONTAL, false)
+        }
+
+        // Syncing RecyclerView with ViewPager2
+        binding.viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                recycler.setSelectedPosition(position)
+                recycler.smoothScrollToSelectedPosition(position)
+            }
+        })
+
+    }
+
+
+}
