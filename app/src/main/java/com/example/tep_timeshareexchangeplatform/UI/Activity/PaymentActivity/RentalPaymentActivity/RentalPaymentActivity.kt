928c6b07725401d1c86866186f77dd0e1c97be3c
quyettc1112 <TranQuyet02588x1112002@gmail.com>
2024-09-29 18:07:45 +0700

    Update Payment Screen 1

---
+++ app/src/main/java/com/example/tep_timeshareexchangeplatform/UI/Activity/PaymentActivity/RentalPaymentActivity/RentalPaymentActivity.kt
@@ -0,0 +1,26 @@
+package com.example.tep_timeshareexchangeplatform.UI.Activity.PaymentActivity.RentalPaymentActivity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.tep_timeshareexchangeplatform.AppConfig.BaseConfig.BaseActivity
+import com.example.tep_timeshareexchangeplatform.R
+import com.example.tep_timeshareexchangeplatform.databinding.ActivityRentalPaymentBinding
+
+class RentalPaymentActivity : BaseActivity() {
+    private lateinit var binding: ActivityRentalPaymentBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityRentalPaymentBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
