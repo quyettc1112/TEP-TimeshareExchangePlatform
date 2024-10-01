c42e41a9666d53cd467dee9ddc46ccb88f0855b0
quyettc1112 <TranQuyet02588x1112002@gmail.com>
2024-10-01 22:39:19 +0700

    Done UI My Order

---
+++ app/src/main/java/com/example/tep_timeshareexchangeplatform/UI/Activity/SubscriptionActivity/MemberShipActivity/MemberShipActivity.kt
@@ -0,0 +1,21 @@
+package com.example.tep_timeshareexchangeplatform.UI.Activity.SubscriptionActivity.MemberShipActivity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.tep_timeshareexchangeplatform.R
+
+class MemberShipActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_member_ship)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/example/tep_timeshareexchangeplatform/UI/Activity/SubscriptionActivity/PackgkageActivity/PackageActivity.kt
@@ -0,0 +1,21 @@
+package com.example.tep_timeshareexchangeplatform.UI.Activity.SubscriptionActivity.PackgkageActivity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.tep_timeshareexchangeplatform.R
+
+class PackageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_package)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
