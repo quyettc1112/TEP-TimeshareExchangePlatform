5f4c985e01b1e0652ce75f2084ada19165871226
QuyetTC <133133145+quyettc1112@users.noreply.github.com>
2024-10-07 12:50:32 +0700

    Update Rental Posting UI

--- app/src/main/java/com/example/tep_timeshareexchangeplatform/UI/Activity/FlowPosting/RentalPostingActivity/Fragment/Step_3_SelectTimeshareFragment.kt
+++ app/src/main/java/com/example/tep_timeshareexchangeplatform/UI/Activity/FlowPosting/RentalPostingActivity/Fragment/Step_3_SelectTimeshareFragment.kt
@@ -5,56 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.tep_timeshareexchangeplatform.AppConfig.BaseConfig.BaseFragment
 import com.example.tep_timeshareexchangeplatform.R
+import com.example.tep_timeshareexchangeplatform.databinding.FragmentSelectTimeshareBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class Step_3_SelectTimeshareFragment : BaseFragment(R.layout.fragment_select_timeshare) {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Step_3_SelectTimeshareFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class Step_3_SelectTimeshareFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding: FragmentSelectTimeshareBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentSelectTimeshareBinding.inflate(layoutInflater, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_select_timeshare, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Step_3_SelectTimeshareFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Step_3_SelectTimeshareFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
