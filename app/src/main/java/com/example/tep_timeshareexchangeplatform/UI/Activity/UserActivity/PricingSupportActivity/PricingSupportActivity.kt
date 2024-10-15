346579d9296ebdb3e26084776730b5c3f1d226db
quyettc1112 <TranQuyet02588x1112002@gmail.com>
2024-10-15 15:08:28 +0700

    Update UI Enter Pricing Support

---
+++ app/src/main/java/com/example/tep_timeshareexchangeplatform/UI/Activity/UserActivity/PricingSupportActivity/PricingSupportActivity.kt
@@ -0,0 +1,26 @@
+package com.example.tep_timeshareexchangeplatform.UI.Activity.UserActivity.PricingSupportActivity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.tep_timeshareexchangeplatform.AppConfig.BaseConfig.BaseActivity
+import com.example.tep_timeshareexchangeplatform.R
+import com.example.tep_timeshareexchangeplatform.databinding.ActivityPricingSupportBinding
+
+class PricingSupportActivity : BaseActivity() {
+    private lateinit var binding: ActivityPricingSupportBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityPricingSupportBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
