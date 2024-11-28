fef1053e7132233c1fbe3adf74c558a271bf3ac3
datnt <datntdev02@gmail.com>
2024-11-28 19:03:58 +0700

    update

--- app/src/main/java/com/example/tep_timeshareexchangeplatform/UI/Activity/MainActivity/Fragment/AccountFragment/AccountFragment.kt
+++ app/src/main/java/com/example/tep_timeshareexchangeplatform/UI/Activity/MainActivity/Fragment/AccountFragment/AccountFragment.kt
@@ -21,6 +21,7 @@ import com.example.tep_timeshareexchangeplatform.UI.Activity.CommonActivity.Noti
 import com.example.tep_timeshareexchangeplatform.UI.Activity.MemberShipActivity.MemberShipActivity
 import com.example.tep_timeshareexchangeplatform.UI.Activity.MainActivity.MainViewModel
 import com.example.tep_timeshareexchangeplatform.UI.Activity.Payment.DepositActivity.DepositActivity
+import com.example.tep_timeshareexchangeplatform.UI.Activity.UserActivity.MyDashboardActivity.MyDashboardActivity
 import com.example.tep_timeshareexchangeplatform.UI.Activity.UserActivity.MyExchangePostingActivity.MyExchangePostings.MyExchangePostingActivity
 import com.example.tep_timeshareexchangeplatform.UI.Activity.UserActivity.MyInfoActivity.MyInfoActivity
 import com.example.tep_timeshareexchangeplatform.UI.Activity.UserActivity.MyOrderActivity.MyOrderActivity
@@ -98,6 +99,7 @@ class AccountFragment : BaseFragment(R.layout.fragment_account) {
                     llCustomerContainer.visibility = View.VISIBLE
 
                     llMyTimeshare.visibility = View.VISIBLE
+                    llMyDashboard.visibility = View.VISIBLE
                     llMyPosting.visibility = View.VISIBLE
                     llMyExchangePosting.visibility = View.VISIBLE
                     llMyTransaction.visibility = View.VISIBLE
@@ -128,6 +130,7 @@ class AccountFragment : BaseFragment(R.layout.fragment_account) {
                     animMembership.visibility = View.GONE
 
                     llMyTimeshare.visibility = View.VISIBLE
+                    llMyDashboard.visibility = View.VISIBLE
                     llMyPosting.visibility = View.VISIBLE
                     llMyExchangePosting.visibility = View.VISIBLE
                     llMyTransaction.visibility = View.VISIBLE
@@ -150,6 +153,7 @@ class AccountFragment : BaseFragment(R.layout.fragment_account) {
                     animMembership.visibility = View.GONE
 
                     llMyTimeshare.visibility = View.GONE
+                    llMyDashboard.visibility = View.GONE
                     llMyPosting.visibility = View.GONE
                     llMyExchangePosting.visibility = View.GONE
                     llMyTransaction.visibility = View.GONE
@@ -253,6 +257,16 @@ class AccountFragment : BaseFragment(R.layout.fragment_account) {
                     Intent(
                         requireContext(),
                         MyTimeshareActivity::class.java
+                    )
+                )
+            }
+
+            // Timeshare của tôi
+            llMyDashboard.setOnClickListener {
+                startActivity(
+                    Intent(
+                        requireContext(),
+                        MyDashboardActivity::class.java
                     )
                 )
             }
