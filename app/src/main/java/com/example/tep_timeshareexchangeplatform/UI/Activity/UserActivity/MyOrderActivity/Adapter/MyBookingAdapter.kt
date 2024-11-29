20445a8bf15b998e790fca5583eb4d5d10757cd3
quyettc1112 <TranQuyet02588x1112002@gmail.com>
2024-11-29 21:05:37 +0700

    Update UI

--- app/src/main/java/com/example/tep_timeshareexchangeplatform/UI/Activity/UserActivity/MyOrderActivity/Adapter/MyBookingAdapter.kt
+++ app/src/main/java/com/example/tep_timeshareexchangeplatform/UI/Activity/UserActivity/MyOrderActivity/Adapter/MyBookingAdapter.kt
@@ -14,26 +14,23 @@ import com.example.tep_timeshareexchangeplatform.Common.Constant
 import com.example.tep_timeshareexchangeplatform.R
 import com.example.tep_timeshareexchangeplatform.Until.EmumClass.MyBookingStatus
 import com.example.tep_timeshareexchangeplatform.databinding.ItemMyBookingBinding
+import com.example.tep_timeshareexchangeplatform.databinding.ItemMyBookinglistBinding
 
 class MyBookingAdapter : BaseAdapter<MyBookingResponse.Content, MyBookingAdapter.MyOrderViewHolder>() {
 
     var onItemClick: ((MyBookingResponse.Content) -> Unit)? = null
     var onFeedbackClick: ((MyBookingResponse.Content) -> Unit)? = null
 
-    inner class MyOrderViewHolder(binding: ItemMyBookingBinding) :
-        BaseItemViewHolderCF<MyBookingResponse.Content, ItemMyBookingBinding>(binding) {
+    inner class MyOrderViewHolder(binding: ItemMyBookinglistBinding) :
+        BaseItemViewHolderCF<MyBookingResponse.Content, ItemMyBookinglistBinding>(binding) {
         override fun bind(item: MyBookingResponse.Content) {
 
 
-            binding.tvTimeshareName.text = "${item.resortName} | ${item.unitTypeTitle}"
+            binding.tvTimeshareName.text = "${item.resortName}"
             binding.tvCheckinDate.text =
-                item.checkinDate?.let { Constant.getFormattedDate(it, binding.root.context) }
-            binding.tvCheckinDayOfWeek.text =
-                item.checkinDate?.let { Constant.getDayOfWeek(it, binding.root.context) }
+                item.checkinDate?.let { Constant.formatDateByLocale(it, binding.root.context) }
             binding.tvCheckoutDate.text =
-                item.checkoutDate?.let { Constant.getFormattedDate(it, binding.root.context) }
-            binding.tvCheckoutDayOfWeek.text =
-                item.checkoutDate?.let { Constant.getDayOfWeek(it, binding.root.context) }
+                item.checkoutDate?.let { Constant.formatDateByLocale(it, binding.root.context) }
             binding.tvTimeshareType.text = item.unitTypeTitle
             Glide.with(binding.root.context).load(item.logo).into(binding.imImageTimeshare)
 
@@ -48,7 +45,8 @@ class MyBookingAdapter : BaseAdapter<MyBookingResponse.Content, MyBookingAdapter
 
 
             /*${binding.root.context.getString(R.string.guests)}*/
-            binding.tvGuestInfo.text = "Khách: ${item.primaryGuestName}"
+            binding.tvGuestEmail.text = "${item.primaryGuestEmail}"
+            binding.tvGuestPhone.text = "SĐT: ${item.primaryGuestPhone}"
 
             when (MyBookingStatus.fromApiStatus(item.status)) {
                 MyBookingStatus.BOOKED -> {
@@ -57,7 +55,6 @@ class MyBookingAdapter : BaseAdapter<MyBookingResponse.Content, MyBookingAdapter
                         R.color.primaryColor,
                         R.color.white
                     )
-                    binding.llFeedbackContainer.visibility = View.GONE
                 }
                 MyBookingStatus.CHECK_IN -> {
                     applyStatusStyle(
@@ -65,7 +62,6 @@ class MyBookingAdapter : BaseAdapter<MyBookingResponse.Content, MyBookingAdapter
                         R.color.blue_btn_search,
                         R.color.white
                     )
-                    binding.llFeedbackContainer.visibility = View.GONE
                 }
                 MyBookingStatus.CHECKOUT -> {
                     applyStatusStyle(
@@ -73,7 +69,6 @@ class MyBookingAdapter : BaseAdapter<MyBookingResponse.Content, MyBookingAdapter
                         R.color.green_verify,
                         R.color.white
                     )
-                    binding.llFeedbackContainer.visibility = View.VISIBLE
                 }
                 MyBookingStatus.NO_SHOW -> {
                     applyStatusStyle(
@@ -81,7 +76,6 @@ class MyBookingAdapter : BaseAdapter<MyBookingResponse.Content, MyBookingAdapter
                         R.color.status_unknown_bg,
                         R.color.status_unknown_text
                     )
-                    binding.llFeedbackContainer.visibility = View.GONE
                 }
                 MyBookingStatus.CANCELED -> {
                     applyStatusStyle(
@@ -89,7 +83,6 @@ class MyBookingAdapter : BaseAdapter<MyBookingResponse.Content, MyBookingAdapter
                         R.color.status_rejected_text,
                         R.color.white
                     )
-                    binding.llFeedbackContainer.visibility = View.GONE
                 }
                 MyBookingStatus.REFUND -> {
                     applyStatusStyle(
@@ -97,7 +90,6 @@ class MyBookingAdapter : BaseAdapter<MyBookingResponse.Content, MyBookingAdapter
                         R.color.status_rejected_text,
                         R.color.white
                     )
-                    binding.llFeedbackContainer.visibility = View.GONE
                 }
                 MyBookingStatus.PAYMENT_COMPLETED -> {
                     applyStatusStyle(
@@ -105,7 +97,6 @@ class MyBookingAdapter : BaseAdapter<MyBookingResponse.Content, MyBookingAdapter
                         R.color.white,
                         R.color.status_pending_approval_text
                     )
-                    binding.llFeedbackContainer.visibility = View.GONE
                 }
                 else -> {
                     // Default or unknown status case
@@ -122,10 +113,6 @@ class MyBookingAdapter : BaseAdapter<MyBookingResponse.Content, MyBookingAdapter
             binding.root.setOnClickListener {
                 onItemClick?.invoke(item)
             }
-            binding.llFeedbackContainer.setOnClickListener {
-                onFeedbackClick?.invoke(item)
-            }
-            binding.llFeedbackContainer.visibility = View.GONE
         }
 
         private fun applyStatusStyle(context: Context, backgroundColorRes: Int, textColorRes: Int) {
@@ -165,7 +152,7 @@ class MyBookingAdapter : BaseAdapter<MyBookingResponse.Content, MyBookingAdapter
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyOrderViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
-        val binding = ItemMyBookingBinding.inflate(layoutInflater, parent, false)
+        val binding = ItemMyBookinglistBinding.inflate(layoutInflater, parent, false)
         return MyOrderViewHolder(binding)
     }
 
