ba5b43f87e91764652fc11bda287ad2cf78d7283
quyettc1112 <TranQuyet02588x1112002@gmail.com>
2024-12-07 14:53:10 +0700

    Update Google Login

--- app/src/main/java/com/example/tep_timeshareexchangeplatform/ZTest/ui/WebView.kt
+++ app/src/main/java/com/example/tep_timeshareexchangeplatform/ZTest/ui/WebView.kt
@@ -11,15 +11,31 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.tep_timeshareexchangeplatform.AppConfig.BaseConfig.BaseActivity
+import com.example.tep_timeshareexchangeplatform.BaseModel.Respone.Customer.Profile.CustomerProfileResponse
+import com.example.tep_timeshareexchangeplatform.BaseModel.Respone.User.LoginResponse
 import com.example.tep_timeshareexchangeplatform.R
+import com.example.tep_timeshareexchangeplatform.UI.Activity.AuthActivity.AuthViewModel.AuthViewModel
+import com.example.tep_timeshareexchangeplatform.UI.Activity.MainActivity.MainActivity
+import com.example.tep_timeshareexchangeplatform.Until.EmumClass.UserLogState
+import com.example.tep_timeshareexchangeplatform.Until.MotionToast.MotionToast
+import com.example.tep_timeshareexchangeplatform.Until.MotionToast.MotionToastStyle
+import com.example.tep_timeshareexchangeplatform.Until.Status
+import com.example.tep_timeshareexchangeplatform.Until.TokenManager.TokenManager
 import com.example.tep_timeshareexchangeplatform.databinding.ActivityWebViewBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class WebView : BaseActivity() {
     private lateinit var binding: ActivityWebViewBinding
+
+    private val authViewModel: AuthViewModel by viewModels()
+    private lateinit var tokenManager: TokenManager
 
     companion object {
         const val UNWIND_OAUTH2 = "https://unwind.id.vn/oauth2/authorization/google"
@@ -36,10 +52,38 @@ class WebView : BaseActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        tokenManager = TokenManager(this)
 
         // Xử lý Deep Link từ Intent
         handleDeepLink(intent)
 
+        observeLoginResponse()
+
+    }
+
+    private fun observeLoginResponse() {
+        authViewModel.profileCustomerInfoResponse.observe(this) { resource ->
+            when (resource.status) {
+                Status.LOADING -> {
+                    // Show a loading spinner
+                    showLoadingWaiting(true)
+                }
+
+                Status.SUCCESS -> {
+                    // Handle success, e.g., navigate to another screen
+                    hideLoadingWaiting()
+                    resource.data?.let { customerInfoResponse ->
+                        handleCheckCustomerExist(customerInfoResponse)
+                    }
+                }
+                Status.ERROR -> {
+                    hideLoadingWaiting()
+                    // SAve user log state, Intent to main
+                    tokenManager.saveUserLogState(UserLogState.LOGGED_IN_AS_USER)
+                    intentToMain()
+                }
+            }
+        }
     }
 
 
@@ -54,11 +98,52 @@ class WebView : BaseActivity() {
             // Lấy giá trị token từ query parameter
             val token = it.getQueryParameter("token")
             if (token != null) {
-                Toast.makeText(this, "Access Token: $token", Toast.LENGTH_LONG).show()
-                // Thực hiện các hành động với token, ví dụ: lưu token hoặc gọi API
+                handleLoginSuccess(token, "")
+                callGetCustomerInfo()
             } else {
                 Toast.makeText(this, "No token received", Toast.LENGTH_LONG).show()
             }
         }
     }
+
+    // Handle login success
+    private fun handleLoginSuccess(accessToken: String, refreshToken: String) {
+        MotionToast.Companion.createColorToast(
+            this,
+            "${getString(R.string.success_login)}",
+            "${getString(R.string.success_login_description)}",
+            MotionToastStyle.SUCCESS,
+            MotionToast.GRAVITY_BOTTOM,
+            MotionToast.LONG_DURATION,
+            ResourcesCompat.getFont(this, R.font.inter_bold)
+        );
+
+        // Save tokens
+        tokenManager.saveTokens(accessToken, refreshToken)
+        tokenManager.saveUserLogState(UserLogState.LOGGED_IN_AS_USER)
+
+    }
+
+    private fun handleCheckCustomerExist(customerInfoResponse : CustomerProfileResponse) {
+        tokenManager.saveProfileInfo(customerInfoResponse)
+        if (customerInfoResponse.isMember) {
+            tokenManager.saveUserLogState(UserLogState.LOGGED_IN_AS_CUSTOMER_MEMBER)
+        } else {
+            tokenManager.saveUserLogState(UserLogState.LOGGED_IN_AS_CUSTOMER)
+        }
+        intentToMain()
+    }
+
+    private fun callGetCustomerInfo() {
+        authViewModel.getProfileCustomerInfo(tokenManager.getAccessToken().toString())
+    }
+
+    private fun intentToMain() {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        startActivity(intent)
+    }
+
+
+
 }
